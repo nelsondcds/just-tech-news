79d239462be897590148ad77f10778fc62bd7b67
nelsondcds <pipoca012008@yahoo.com>
2021-09-05 19:00:07 -0400

    added login and signup pages and functionality

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -7,7 +7,15 @@ router.get('/', (req, res) => {
     User.findAll({
         attributes: { exclude: ['password'] }
     })
-    .then(dbUserData => res.json(dbUserData))
+    .then(dbUserData => {
+        req.session.save(() => {
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+        
+            res.json(dbUserData);
+        });
+    })
     .catch(err => {
         console.log(err);
         res.status(500).json(err);
@@ -114,11 +122,26 @@ router.post('/login', (req, res) => {
             return;
         }
         
-        res.json({ user: dbUserData, message: 'You are now logged in!' });
-
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
+            req.session.username = dbUserData.username;
+            req.session.loggedIn = true;
+      
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
+        });
     });  
-  
 })
+
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
 // DELETE /api/users/1
 router.delete('/:id', (req, res) => {
